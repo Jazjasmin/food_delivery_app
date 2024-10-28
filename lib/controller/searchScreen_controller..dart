2865c82dc144a99d69811978c60fb40cd1f084f9
JasminSRV <jasmin@srvinfotech.in>
2024-10-28 10:14:35 +0400

    first commit

---
+++ lib/controller/searchScreen_controller..dart
@@ -0,0 +1,21 @@
+import 'package:get/get.dart';
+
+class SearchController extends GetxController {
+// <<<<<<< HEAD
+// =======
+  
+// >>>>>>> 650b1a9e7d822cdd9131b07132a710ffa5dc6e0d
+  var initialFoodSelected = 0.obs;
+  var initialCategorySelected = 0.obs;
+
+  void mealButtonSwitch(int index) {
+    initialFoodSelected.value = index;
+  }
+
+  // categorySelection
+
+  void foodButtonSwitch(int index) {
+    initialCategorySelected.value = index;
+  }
+  // Griditems
+}
---
+++ lib/helper/bindings.dart
@@ -0,0 +1,9 @@
+// import 'package:food_delivery_app_ui/controller/auth_controller.dart';
+// import 'package:get/get.dart';
+
+// class MyControllerBindings extends Bindings {
+//   @override
+//   void dependencies() {
+//     Get.lazyPut<AuthController>(() => AuthController());
+//   }
+// }
